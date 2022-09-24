ade15fc856ff96554ad2e59e3a049e426b7b8f03
Fidelis Amadi <108342029+Delian21@users.noreply.github.com>
2022-09-24 20:48:51 +0100

    Add files via upload

---
+++ Parallax Page Dev-Design/main.js
@@ -0,0 +1,17 @@
+let cloud = document.getElementById('cloud')
+let mountains_behind = document.getElementById('mountains-behind')
+let text = document.getElementById('text')
+let btn = document.getElementById('btn')
+let mountains_front = document.getElementById('mountains-front')
+let header = document.querySelector('header')
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    cloud.style.left = value * 0.5 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4.35 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.6 + 'px';
+    header.style.top = value * 0.5 + 'px';
+})
